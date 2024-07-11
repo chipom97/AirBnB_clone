bca77357dbce710db31d16c8670eb6faa542046c
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-11 19:29:13 +0000

    task_4

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,19 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of the AirBnB Console"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new Basemodel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
+         if kwargs:
+             for key, value in kwargs.items():
+                 if key != '__class__':
+                     if key in ('created_at', 'updated_at'):
+                         value = datetime.fromisoformat(value)
+                         setattr(self, key, value)
+         else:
+             self.id = str(uuid.uuid4())
+             self.created_at = datetime.now()
+             self.updated_at = self.created_at
+        
     def __str__(self):
         """Return the string representation of a BaseModel instance"""
         class_name = self.__class__.__name__
@@ -25,6 +32,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance"""
         dict_rep = self.__dict__.copy()
         dict_rep['__class__'] = self.__class__.__name__
         dict_rep['created_at'] = self.created_at.isoformat()
