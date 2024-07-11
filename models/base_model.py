7bf2ad956961db3948b3244e130d561c69755599
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-11 20:06:15 +0000

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -19,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """Return the string representation of a BaseModel instance"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Define the File Storage Class"""
+
+import json
+import os
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (if it exists)"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name = key.split('.')[0]
+                    cls = globals()[class_name]
+                    self.__objects[key] = cls(**value)
