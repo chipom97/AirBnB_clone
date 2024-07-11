0f0377784d49f0e4963e730a9f9a3a9bc2aec946
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-11 14:43:42 +0000

    removed files

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-a convenient and organized way to initialize a package and its modules
-"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
