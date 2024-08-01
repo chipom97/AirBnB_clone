8701cbe517f20f06a808f6998ae4c90d1e5d9b90
Chipo Mazhani <chipomazhani@gmail.com>
2024-08-01 19:13:27 +0000

    unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 import sys
 import os
 
@@ -11,14 +10,54 @@ project_root = os.path.abspath(os.path.join(script_dir, '../../'))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+
+
+    def test_id(self):
+        model = BaseModel()
+        self.assertEqual(type(model.id), str)
+        self.assertEqual(len(model.id), 36)
+
+    def test_created_at(self):
+        model = BaseModel()
+        self.assertEqual(type(model.created_at), datetime)
+
+    def test_updated_at(self):
+        model = BaseModel()
+        self.assertEqual(type(model.updated_at), datetime)
+        old_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(model.updated_at, old_updated_at)
+
+    def test_str(self):
+        model = BaseModel()
+        expected = f"[BaseModel] ({model.id}) {model.__dict__}"
+        self.assertEqual(str(model), expected)
+
+    def test_to_dict(self):
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
+    def test_init_with_kwargs(self):
+        model_dict = {
+            'id': '123e4567-e89b-12d3-a456-426614174000',
+            'created_at': '2024-07-11T14:58:00.123456',
+            'updated_at': '2024-07-11T14:58:00.123456',
+            'name': 'MyModel'
+        }
+        model_instance = BaseModel(**model_dict)
+        self.assertEqual(model_instance.id, '123e4567-e89b-12d3-a456-426614174000')
+        self.assertEqual(model_instance.created_at, datetime.fromisoformat('2024-07-11T14:58:00.123456'))
+        self.assertEqual(model_instance.updated_at, datetime.fromisoformat('2024-07-11T14:58:00.123456'))
+        self.assertEqual(model_instance.name, 'MyModel')
+
+
+if __name__ == '__main__':
+    unittest.main()
